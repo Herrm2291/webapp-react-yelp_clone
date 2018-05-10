1b7f4820cee901917e02b6a14a6de9cb82b0e8a7
Steve Herrmann <37193114+Herrm2291@users.noreply.github.com>
2018-05-10 18:30:05 -0400

    created Yelp module, added search method and added access to the jsonResponse object returned by the Yelp api

--- yelp_clone/src/util/Yelp.js
+++ yelp_clone/src/util/Yelp.js
@@ -1,1 +1,32 @@
 const apiKey = 'qXcdWEUReAibH1ke59f53-c4FiZhWhQuaYAgs-KM-59FzVIKDw2cNDn6WfMl8yrjwItDwpMOJbyaG643ZRNnLaHOa7eDixt9x8-mNEEhNIKEsb9lxNtzx5hcX7z0WnYx';
+
+const Yelp = {};
+
+/* method to return a promise that will ultimately resolve to the list of businesses */
+search(term, location, sortBy) {
+  /* CORS (cross-origin resource sharing) bypass https://cors-anywhere.herokuapp.com/...*/
+  return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sort_by}`, {
+    headers: {
+      { Authorization: `Bearer ${apiKey}` }
+    }
+  }).then(response => {
+    return response.json();
+  }).then(jsonResponse => {
+    if (jsonResponse.businesses) {
+      return jsonResponse.businesses.map(business => {
+        id: business.id,
+        imageSrc: business.image_url,
+        name: business.name,
+        address: business.address1,
+        city: business.city,
+        state: business.state,
+        zipCode: business.zip_code,
+        category: category.title,
+        rating: business.rating,
+        reviewCount:business.review_count
+      });
+    }
+  });
+}
+
+export default Yelp;
